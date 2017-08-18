889a4478e40c9630ca7af055744ec08eb3b1e52a
Ethan Bao <cbao@google.com>
2017-08-18 16:23:43 -0700

    Add converter cmd to convert from legacy artman config into the new format

--- artman/config/converter.py
+++ artman/config/converter.py
@@ -43,7 +43,7 @@ def convert_to_legacy_config_dict(artifact_config, intput_dir, output_dir):
         _proto_deps_to_legacy_configs(artifact_config.proto_deps,
                                       artifact_config.test_proto_deps))
     common['proto_deps'] = legacy_proto_deps
-    common['test_proto_deps'] = legacy_test_proto_deps
+    common['proto_test_deps'] = legacy_test_proto_deps
     common['desc_proto_path'] = desc_proto_paths
 
     package_type = 'grpc_client'  # default package_type
@@ -64,9 +64,10 @@ def convert_to_legacy_config_dict(artifact_config, intput_dir, output_dir):
         output_dir, rel_gapic_code_dir)
     language_config_dict['git_repos'] = _calculate_git_repos_config(
         artifact_config, output_dir)
-    language_config_dict['release_level'] = (
-        Artifact.ReleaseLevel.Name(
-            artifact_config.release_level).lower())
+    if artifact_config.release_level != Artifact.RELEASE_LEVEL_UNSPECIFIED:
+        language_config_dict['release_level'] = (
+            Artifact.ReleaseLevel.Name(
+                artifact_config.release_level).lower())
 
     # Convert package version configuration.
     pv = artifact_config.package_version
