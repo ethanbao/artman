adfa81d8c2b3ebe5398ad4998b8bc552970c78a3
Ethan Bao <cbao@google.com>
2017-08-21 13:51:51 -0700

    Update smoketest_artman.py to test with the new artman configs.

--- artman/cli/converter.py
+++ artman/cli/converter.py
@@ -30,7 +30,7 @@ import yaml
 
 from collections import OrderedDict
 
-from artman.config.proto import config_pb2
+from artman.config.proto.config_pb2 import Artifact, Config
 from google.protobuf.json_format import MessageToJson
 
 
@@ -39,10 +39,14 @@ def main(*args):
         args = sys.argv[1:]
 
     # Get to a normalized set of arguments.
-    flags = parse_args(*args)
-    legacy_config = _load_legacy_config_dict(os.path.abspath(flags.config))
-    new_config = _convert(legacy_config)
-    _write_pb_to_yaml(new_config, flags.output)
+    try:
+        flags = parse_args(*args)
+        legacy_config = _load_legacy_config_dict(os.path.abspath(flags.config))
+        new_config = _convert(legacy_config)
+        _write_pb_to_yaml(new_config, flags.output)
+    except Exception:
+        print('Fail to convert `%s`.' % flags.config)
+        raise
 
 
 def parse_args(*args):
@@ -55,7 +59,7 @@ def parse_args(*args):
         '--output',
         type=str,
         default=None,
-        help='If specified, the converted yaml will be stored in the specified '
+        help='If specified, the converted yaml will be stored in the given '
              'output file. Otherwise, the converter will only print out the '
              'result.')
     return parser.parse_args(args=args)
@@ -63,78 +67,99 @@ def parse_args(*args):
 
 def _convert(legacy_config):
     # Compute common section
-    result = config_pb2.Config()
-    result.common.api_name = legacy_config['common']['api_name']
-    result.common.api_version = legacy_config['common']['api_version']
-    result.common.organization_name = legacy_config[
-        'common']['organization_name']
-    if 'gapic_api_yaml' inlegacy_config['common']:
+    result = Config()
+    if 'common' not in legacy_config:
+        print('`common` field is a required field in the legacy config.')
+        sys.exit(2)
+    legacy_common = legacy_config.get('common')
+    result.common.api_name = legacy_common.get('api_name', 'unspecified')
+    result.common.api_version = legacy_common.get('api_version', 'unspecified')
+    result.common.organization_name = legacy_common.get(
+        'organization_name', 'unspecified')
+
+    # Python dict#get(key, default=value) doesn't well support list-type field.
+    # Instead of using defaultdict, simply do the if checking here.
+    if legacy_common.get('gapic_api_yaml'):
         result.common.gapic_yaml = _sanitize_repl_var(
-            legacy_config['common']['gapic_api_yaml'][0])
-    result.common.service_yaml = _sanitize_repl_var(
-        legacy_config['common']['service_yaml'][0])
-    if 'proto_deps' in legacy_config['common']:
-        result.common.proto_deps.extend(_compute_deps(
-            legacy_config['common']['proto_deps']))
-    if 'proto_test_deps' in legacy_config['common']:
-        result.common.test_proto_deps.extend(_compute_deps(
-            legacy_config['common']['proto_test_deps']))
-    result.common.src_proto_paths.extend(
-        _compute_src_proto_paths(legacy_config['common']['src_proto_path']))
+            legacy_common.get('gapic_api_yaml')[0])
+    if legacy_common.get('service_yaml'):
+        result.common.service_yaml = _sanitize_repl_var(
+            legacy_common.get('service_yaml')[0])
+    if legacy_common.get('proto_deps'):
+        result.common.proto_deps.extend(
+            _compute_deps(legacy_common.get('proto_deps')))
+    if legacy_common.get('proto_test_deps'):
+        result.common.test_proto_deps.extend(
+            _compute_deps(legacy_common.get('proto_test_deps')))
+    if legacy_common.get('src_proto_path'):
+        result.common.src_proto_paths.extend(
+            _compute_src_proto_paths(legacy_common.get('src_proto_path')))
+
+    result.artifacts.extend(_compute_artifacts(legacy_config, legacy_common))
+    return result
+
+
+def _compute_artifacts(legacy_config, legacy_common):
+    result = []
+    LANGS = ['java', 'python', 'php', 'ruby', 'go', 'csharp', 'nodejs']
 
     # Compute artifacts section
-    common_git_repos = legacy_config['common']['git_repos']
-    LANGS = ['java', 'python', 'php', 'ruby', 'go', 'csharp', 'nodejs']
+    if legacy_common.get('git_repos'):
+        common_git_repos = legacy_common.get('git_repos')
+    else:
+        common_git_repos = []
+
     for lang in LANGS:
         if lang not in legacy_config:
             continue
         legacy_artifact_config = legacy_config[lang]
         if legacy_artifact_config:
-            artifact = config_pb2.Artifact()
+            artifact = Artifact()
+            # Name the artifact as `{lang}_gapic`.
             artifact.name = '%s_gapic' % lang
-            artifact.language = config_pb2.Artifact.Language.Value(lang.upper())
+            artifact.language = Artifact.Language.Value(lang.upper())
 
             if 'release_level' in legacy_artifact_config:
-                artifact.release_level = config_pb2.Artifact.ReleaseLevel.Value(
-                    legacy_artifact_config['release_level'].upper())
+                artifact.release_level = Artifact.ReleaseLevel.Value(
+                    legacy_artifact_config.get('release_level').upper())
 
-            artifact.type = _compute_artifact_type(legacy_config)
+            artifact.type = _compute_artifact_type(legacy_common)
 
             # Compute package version.
             if 'generated_package_version' in legacy_artifact_config:
-                legacy_package_version = legacy_artifact_config[
-                    'generated_package_version']
+                legacy_package_version = legacy_artifact_config.get(
+                    'generated_package_version')
                 if 'lower' in legacy_package_version:
                     artifact.package_version.grpc_dep_lower_bound = (
-                        legacy_package_version['lower'])
+                        legacy_package_version.get('lower'))
                 if 'upper' in legacy_package_version:
                     artifact.package_version.grpc_dep_upper_bound = (
-                        legacy_package_version['upper'])
+                        legacy_package_version.get('upper'))
 
             # Compute publishing targets
             if 'git_repos' in legacy_artifact_config:
                 artifact.publish_targets.extend(_compute_publish_targets(
-                    legacy_artifact_config['git_repos'], common_git_repos))
-            result.artifacts.extend([artifact])
-
+                    legacy_artifact_config.get('git_repos'), common_git_repos))
+            result.append(artifact)
     return result
+
 
 def _compute_package_version(legacy_package_version):
-    result = config_pb2.Artifact.PackageVersion()
+    result = Artifact.PackageVersion()
     if 'lower' in legacy_package_version:
-        result.grpc_dep_lower_bound = legacy_package_version['lower']
+        result.grpc_dep_lower_bound = legacy_package_version.get('lower')
     if 'upper' in legacy_package_version:
-        result.grpc_dep_upper_bound = legacy_package_version['upper']
+        result.grpc_dep_upper_bound = legacy_package_version.get('upper')
     return result
 
 
-def _compute_artifact_type(legacy_config):
-    if legacy_config['common']['packaging'] == 'google-cloud':
-        return config_pb2.Artifact.GAPIC_ONLY
-    elif legacy_config['common']['package_type'] == 'grpc_common':
-        return config_pb2.Artifact.GRPC_COMMON
+def _compute_artifact_type(legacy_common):
+    if legacy_common.get('packaging', '') == 'google-cloud':
+        return Artifact.GAPIC_ONLY
+    elif legacy_common.get('package_type', '') == 'grpc_common':
+        return Artifact.GRPC_COMMON
 
-    return config_pb2.Artifact.GAPIC
+    return Artifact.GAPIC
 
 
 def _compute_publish_targets(git_repos, common_git_repos):
@@ -142,10 +167,10 @@ def _compute_publish_targets(git_repos, common_git_repos):
     for k, v in git_repos.items():
         location = (common_git_repos[k]['location']
                     if k in common_git_repos.keys() else v['location'])
-        target = config_pb2.Artifact.PublishTarget()
+        target = Artifact.PublishTarget()
         target.name = k
         target.location = location
-        target.type = config_pb2.Artifact.PublishTarget.GITHUB
+        target.type = Artifact.PublishTarget.GITHUB
 
         if 'paths' in v:
             target.directory_mappings.extend(
@@ -153,29 +178,32 @@ def _compute_publish_targets(git_repos, common_git_repos):
         result.append(target)
     return result
 
+
 def _compute_directory_mappings(paths):
     result = []
     for path in paths:
-      mapping = config_pb2.Artifact.PublishTarget.DirectoryMapping()
-      if isinstance(path, str):
-          mapping.dest = path
-      else:
-          if 'src' in path:
-              mapping.src = path['src']
-          if 'dest' in path:
-              mapping.dest = path['dest']
-          if 'artifact' in path:
-              mapping.name = path['artifact']
-          result.append(mapping)
+        mapping = Artifact.PublishTarget.DirectoryMapping()
+        if isinstance(path, str):
+            mapping.dest = path
+        else:
+            if 'src' in path:
+                mapping.src = path.get('src')
+            if 'dest' in path:
+                mapping.dest = path.get('dest')
+            if 'artifact' in path:
+                mapping.name = path.get('artifact')
+            result.append(mapping)
     return result
+
 
 def _compute_deps(proto_deps):
     result = []
     for dep in proto_deps:
-        new_dep = config_pb2.Artifact.ProtoDependency()
+        new_dep = Artifact.ProtoDependency()
         new_dep.name = dep
         result.append(new_dep)
     return result
+
 
 def _compute_src_proto_paths(src_proto_paths):
     result = []
@@ -183,15 +211,18 @@ def _compute_src_proto_paths(src_proto_paths):
         result.append(_sanitize_repl_var(src_proto_path))
     return result
 
+
 def _sanitize_repl_var(value):
     if value.startswith('${GOOGLEAPIS}/'):
         return value.replace('${GOOGLEAPIS}/', '')
     if value.startswith('${REPOROOT}/'):
         return value.replace('${REPOROOT}/', '')
 
+
 def camel_to_underscore(name):
     camel_pat = re.compile(r'([A-Z])')
     return camel_pat.sub(lambda x: '_' + x.group(1).lower(), name)
+
 
 def convert_json(d):
     """Convert the dict to turn all key into lower underscore case."""
@@ -211,6 +242,7 @@ def convert_json(d):
             new_d[camel_to_underscore(k)] = v
     return new_d
 
+
 def _write_pb_to_yaml(pb, output):
     # Add yaml representer so taht yaml dump can dump OrderedDict. The code
     # is coming from https://stackoverflow.com/questions/16782112.
@@ -224,6 +256,7 @@ def _write_pb_to_yaml(pb, output):
     else:
         print(yaml.dump(json_obj, default_flow_style=False))
 
+
 def represent_ordereddict(dumper, data):
     value = []
     for item_key, item_value in data.items():
@@ -233,6 +266,7 @@ def represent_ordereddict(dumper, data):
         value.append((node_key, node_value))
 
     return yaml.nodes.MappingNode(u'tag:yaml.org,2002:map', value)
+
 
 def _order_dict(od):
     # The whole key order is flattened which is okay for artman config because
@@ -247,7 +281,7 @@ def _order_dict(od):
         'grpc_dep_lower_bound', 'grpc_dep_upper_bound'
     ]
     res = OrderedDict()
-    for k, v in sorted(od.items(), key=lambda i:keyorder.index(i[0])):
+    for k, v in sorted(od.items(), key=lambda i: keyorder.index(i[0])):
         if isinstance(v, dict):
             res[k] = _order_dict(v)
         elif isinstance(v, list):
@@ -262,9 +296,11 @@ def _order_dict(od):
             res[k] = v
     return res
 
+
 def _load_legacy_config_dict(path):
     with io.open(path, 'r') as yaml_file:
         return yaml.load(yaml_file)
+
 
 if __name__ == "__main__":
     main()
