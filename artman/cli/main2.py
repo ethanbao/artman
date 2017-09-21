6a0acedab868e88c036e38a045268d8d27f9741e
Ethan Bao <cbao@google.com>
2017-09-21 13:56:39 -0700

    bump the artman version to 0.4.18 as 0.4.17 has been used

--- artman/cli/main2.py
+++ artman/cli/main2.py
@@ -40,7 +40,7 @@ from artman.pipelines import pipeline_factory
 from artman.utils import config_util
 from artman.utils.logger import logger, setup_logging
 
-ARTMAN_DOCKER_IMAGE = 'googleapis/artman:0.4.17'
+ARTMAN_DOCKER_IMAGE = 'googleapis/artman:0.4.18'
 RUNNING_IN_ARTMAN_DOCKER_TOKEN = 'RUNNING_IN_ARTMAN_DOCKER'
 
 
